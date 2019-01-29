6afa88be05b4ce702677b15ad7229d9dd682ee20
Roman Parkhomenko <rsparkhomenko@gmail.com>
2019-01-29 12:07:53 -0700

    resume changes

---
+++ precache-manifest.75d3f6e9ea74b4444c78924396b63617.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b88d46315023abb27a54",
+    "url": "/static/js/main.b88d4631.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "b88d46315023abb27a54",
+    "url": "/static/css/main.140c1842.chunk.css"
+  },
+  {
+    "revision": "b46805f8faed48dbca056d49ba56d175",
+    "url": "/index.html"
+  }
+];
